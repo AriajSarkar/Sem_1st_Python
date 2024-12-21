0da03efdbb062db1867579b60030136cf584df88
Ariaj Sarkar <rajsarkar02205@gmail.com>
2024-12-21 23:17:30 +0530

    5. Add factorial calculator using recursion

---
+++ 5_Factorial/main.py
@@ -0,0 +1,14 @@
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+def main():
+    n = int(input("Enter a number: "))
+    
+    result = factorial(n)
+    print(f"The factorial of {n} is {result}")
+    
+if __name__ == "__main__":
+    main()
