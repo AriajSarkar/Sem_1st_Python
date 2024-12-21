78b85ff18e5d33438a3e0b1ce623859f6913bedc
Ariaj Sarkar <rajsarkar02205@gmail.com>
2024-12-21 23:18:17 +0530

    External changes

--- TempConverter/Cel_Fah/CtoF.py
+++
@@ -1,3 +0,0 @@
-def celsius_to_fahrenheit(celsius: float) -> float:
-    """Convert Celsius to Fahrenheit."""
-    return (celsius * 9.0 / 5.0) + 32
--- TempConverter/Fah_Cel/FtoC.py
+++
@@ -1,3 +0,0 @@
-def fahrenheit_to_celsius(fahrenheit: float) -> float:
-    """Convert Fahrenheit to Celsius."""
-    return (fahrenheit - 32) * 5.0 / 9.0
